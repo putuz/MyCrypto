2badca52e4c882d1ee891e1fc174a0ee453b229f
Putut <pututyb@gmail.com>
2026-06-09 15:24:36 +0700

    Initial Commit

---
+++ MyCrypto/MyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCryptoApp.swift
+//  MyCrypto
+//
+//  Created by Amelia Putri Oktaviani on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
