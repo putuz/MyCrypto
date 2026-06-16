a9a88e7e59c37f4872241598433e7557f2383119
Putut <pututyb@gmail.com>
2026-06-16 11:32:35 +0700

    feat: CoinDetailView and UI

---
+++ MyCrypto/Views/CoinDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinDetailView.swift
+//  MyCrypto
+//
+//  Created by Amelia Putri Oktaviani on 16/06/2026.
+//
+
+import SwiftUI
+
+struct CoinDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinDetailView()
+}
